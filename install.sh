76f75ab09f6f601c225c7cf057eddf4ac8d5bef7
stroopkoek <34483212+stroopkoek@users.noreply.github.com>
2020-02-17 21:37:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,30 +4,35 @@ cat <<-'EOF' > /opt/tplink/stroopwafel/docker_entrypoint.sh
 #declaring variables
 targetdirectory=/opt/tplink/EAPController
 
-#look if configuration exists on disk:
-#if there is; import them by linking them
-#else; link the base files to the current_config
-if [ -f /current_config/bin/control.sh ]; then
-  echo "Found existing files; importing them to disk."
-  rm -rf ${targetdirectory}/bin ${targetdirectory}/data \
-         ${targetdirectory}/keystore ${targetdirectory}/logs \
-         ${targetdirectory}/properties ${targetdirectory}/work
-  ln -fs /current_config/bin $targetdirectory
-  ln -fs /current_config/data $targetdirectory
-  ln -fs /current_config/keystore $targetdirectory
-  ln -fs /current_config/logs $targetdirectory
-  ln -fs /current_config/properties $targetdirectory
-  ln -fs /current_config/work $targetdirectory
-else
-  rm -rf /current_config/*
+#if /current_config/bin/control.sh doesn't exist then
+#remove the current_config just in case and move the system folders to current_config
+#else echo for logging purposes that existing files are found!
+if ![ -f /current_config/bin/control.sh ]; then
+  #remove current_config just in case; move the system folders to current_config
+  #so that the link will work
+
   echo "Configuring new files; exporting them to volume"
-  ln -fs ${targetdirectory}/bin /current_config
-  ln -fs ${targetdirectory}/data /current_config
-  ln -fs ${targetdirectory}/keystore /current_config
-  ln -fs ${targetdirectory}/logs /current_config
-  ln -fs ${targetdirectory}/properties /current_config
-  ln -fs ${targetdirectory}/work /current_config/
+  rm -rf /current_config/*
+  mv ${targetdirectory}/bin /current_config/bin
+  mv ${targetdirectory}/data /current_config/data
+  mv ${targetdirectory}/keystore /current_config/keystore
+  mv ${targetdirectory}/logs /current_config/logs
+  mv ${targetdirectory}/properties /current_config/properties
+  mv ${targetdirectory}/work /current_config/work
+
+else
+  #nothing to do except symlinking in next block
+  echo "Found existing files; importing them to disk."
+
 fi
+
+#symlink it to the EAP folder.
+ln -fs /current_config/bin $targetdirectory
+ln -fs /current_config/data $targetdirectory
+ln -fs /current_config/keystore $targetdirectory
+ln -fs /current_config/logs $targetdirectory
+ln -fs /current_config/properties $targetdirectory
+ln -fs /current_config/work $targetdirectory
 
 #starting tpeap service
 /opt/tplink/EAPController/bin/control.sh start
